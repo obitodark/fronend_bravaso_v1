860febce551efb9502ab0b8ac6595a7130213d66
willians ccallo <mayhixccallo@gmail.com>
2023-02-09 18:22:05 -0500

    add new link de backend

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,3 @@
-export const API_URL = 'https://frozen-tor-02595.herokuapp.com';
+// export const API_URL = 'https://frozen-tor-02595.herokuapp.com';
+
+export const API_URL = 'https://backendbravaso-production.up.railway.app/swagger-ui';
